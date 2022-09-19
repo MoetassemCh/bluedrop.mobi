09638f2537d9a238b00a6c3491ab32beb58aed72
MoetassemCh <103120209+MoetassemCh@users.noreply.github.com>
2022-09-19 19:48:11 +0300

    final4

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -33,6 +33,7 @@ $url=$this->adminUrlGenerator
 ->generateUrl();
 
         return $this->redirect($url);
+
     }
 
     public function configureDashboard(): Dashboard
@@ -46,23 +47,29 @@ $url=$this->adminUrlGenerator
         yield MenuItem::section('Dashboard', 'fa fa-home');
         yield MenuItem::section('Project');
         yield MenuItem::subMenu("Actions",'fas fa-bars')->setSubItems([
-        MenuItem::linkToCrud('Add Project','fas fa-plus',Project::class)
+        MenuItem::linkToCrud('Add Project','fas fa-plus',Project::class)->setAction(Crud::PAGE_NEW),
+        MenuItem::linkToCrud('Show Projects','fas fa-eye',Project::class)
         ]);
         yield MenuItem::section('Orders');
         yield MenuItem::subMenu("Actions", 'fas fa-bars')->setSubItems([
-                MenuItem::linkToCrud('Create Order', 'fas fa-plus', Order::class)
+                MenuItem::linkToCrud('Create Order', 'fas fa-plus', Order::class)->setAction(Crud::PAGE_NEW),
+                MenuItem::linkToCrud('Show Orders', 'fas fa-eye', Order::class)
             ]);
 
         yield MenuItem::section('Ticket');
         yield MenuItem::subMenu("Actions", 'fas fa-bars')->setSubItems([
-                 MenuItem::linkToCrud('Create Ticket', 'fas fa-plus', Ticket::class)
+                 MenuItem::linkToCrud('Create Ticket', 'fas fa-plus', Ticket::class)->setAction(Crud::PAGE_NEW),
+                 MenuItem::linkToCrud('Show Tickets', 'fas fa-eye', Ticket::class)
             ]);
 
              yield MenuItem::section('Notes');
              yield MenuItem::subMenu("Actions", 'fas fa-bars')->setSubItems([
-                 MenuItem::linkToCrud('Create Notes', 'fas fa-plus', Notes::class)
+                 MenuItem::linkToCrud('Create Notes', 'fas fa-plus', Notes::class)->setAction(Crud::PAGE_NEW),
+                 MenuItem::linkToCrud('Show Notes', 'fas fa-eye', Notes::class)
             ]);
 
 
     }
+
+    
 }
