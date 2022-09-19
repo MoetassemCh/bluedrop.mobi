37ca1ff3de37dd469f2f1252b8af75cc74142348
MoetassemCh <103120209+MoetassemCh@users.noreply.github.com>
2022-09-19 20:22:10 +0300

    final8

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -15,7 +15,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
 
-
+#[IsGranted('ROLE_ADMIN')]
 
 class DashboardController extends AbstractDashboardController
 {
@@ -24,16 +24,27 @@ public function __construct(private AdminUrlGenerator $adminUrlGenerator)
     
 }
 
-    #[IsGranted('ROLE_ADMIN')]
     #[Route('/admin', name: 'app_admin')]
     public function index(): Response
     {
 $url=$this->adminUrlGenerator
 ->setController(ProjectCrudController::class)
 ->generateUrl();
-
+        // Option 1. You can make your dashboard redirect to some common page of your backend
+        //
+        // $adminUrlGenerator = $this->container->get(AdminUrlGenerator::class);
         return $this->redirect($url);
 
+        // Option 2. You can make your dashboard redirect to different pages depending on the user
+        //
+        // if ('jane' === $this->getUser()->getUsername()) {
+        //     return $this->redirect('...');
+        // }
+
+        // Option 3. You can render some custom template to display a proper dashboard with widgets, etc.
+        // (tip: it's easier if your template extends from @EasyAdmin/page/content.html.twig)
+        //
+        // return $this->render('some/path/my-dashboard.html.twig');
     }
 
     public function configureDashboard(): Dashboard
@@ -70,6 +81,4 @@ $url=$this->adminUrlGenerator
 
 
     }
-
-    
 }
